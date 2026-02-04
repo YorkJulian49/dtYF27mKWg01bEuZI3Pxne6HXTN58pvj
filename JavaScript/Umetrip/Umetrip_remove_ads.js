88882d73c6876a17969ef4577867a5c9b3705162
York Julian49 <49021108+YorkJulian49@users.noreply.github.com>
2026-02-04 01:30:04 -0800

    Add Umetrip_remove_ads.js to handle ad removal

---
+++ JavaScript/Umetrip/Umetrip_remove_ads.js
@@ -0,0 +1,10 @@
+const version = 'V1.0.1';
+var ua = $request.headers.rpid || $request.headers.Rpid;
+ua.includes("1000002") || ua.includes("1000019") ? $done({
+    status: "HTTP/1.1 404 Not Found"
+}) : $done({});
+const version = 'V1.0.1';
+var ua = $request.headers.rpid || $request.headers.Rpid;
+ua.includes("1000002") || ua.includes("1000019") ? $done({
+    status: "HTTP/1.1 404 Not Found"
+}) : $done({});
